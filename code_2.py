21bea0829b8547f2a4be8231fe0cafdac2eec1bb
NatalyBlack <140597696+NatalyBlack@users.noreply.github.com>
2023-12-03 12:20:16 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,39 @@
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+if __name__ == "__main__":
+    for item in data:
+
+    return data
+def generate_random_data():
+        print(f"Random Number: {item}")
+import random
+    main()
+
+    data = generate_random_data()def generate_random_data():
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+    data = generate_random_data()
+if __name__ == "__main__":
+    main()
+    return data
+
+def main():
+    for item in data:
+        print(f"Random Number: {item}")
+def main():
+def generate_random_data():
+    for item in data:
+
+if __name__ == "__main__":
+    data = generate_random_data()
+
+
+import random
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
